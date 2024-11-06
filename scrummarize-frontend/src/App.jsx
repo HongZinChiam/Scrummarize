15c810266021964fb01f74951f70a471d5e12b97
Ren <ren12death@gmail.com>
2024-11-06 23:15:13 +0800

    feat(product backlog): create ProductBacklog component
    - Add ProductBacklog to App.jsx
    - Add styles for ProductBacklog in ProductBacklog.css
    - Add layout in App

--- scrummarize-frontend/src/App.jsx
+++ scrummarize-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Header from './Header.jsx'
+import ProductBacklog from './ProductBacklog.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <ProductBacklog />
+      </main>
     </>
   )
 }
